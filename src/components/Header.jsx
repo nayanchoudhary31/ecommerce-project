cde7a5b755529be0a6286ec22d80a945e6a99d5a
nayanchoudhary31 <nayanscsit09@gmail.com>
2026-04-02 16:37:07 +0530

    feat: add Header component and HomePage layout with product listings

---
+++ src/components/Header.jsx
@@ -0,0 +1,42 @@
+import { NavLink } from "react-router";
+import "./header.css";
+import CartIcon from "../assets/images/icons/cart-icon.png";
+import SearchIcon from "../assets/images/icons/search-icon.png";
+import LogoWhite from "../assets/images/logo-white.png";
+import MobileLogoWhite from "../assets/images/mobile-logo-white.png";
+function Header() {
+  return (
+    <>
+      <div className="header">
+        <div className="left-section">
+          <NavLink to="/" className="header-link">
+            <img className="logo" src={LogoWhite} />
+            <img className="mobile-logo" src={MobileLogoWhite} />
+          </NavLink>
+        </div>
+
+        <div className="middle-section">
+          <input className="search-bar" type="text" placeholder="Search" />
+
+          <button className="search-button">
+            <img className="search-icon" src={SearchIcon} />
+          </button>
+        </div>
+
+        <div className="right-section">
+          <NavLink to="/orders" className="orders-link header-link">
+            <span className="orders-text">Orders</span>
+          </NavLink>
+
+          <NavLink to="/checkout" className="cart-link header-link">
+            <img className="cart-icon" src={CartIcon} />
+            <div className="cart-quantity">3</div>
+            <div className="cart-text">Cart</div>
+          </NavLink>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
