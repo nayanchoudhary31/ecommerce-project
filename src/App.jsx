91642279be542b98971d9a5fe31896475c7ce6df
nayanchoudhary31 <nayanscsit09@gmail.com>
2026-04-02 16:35:42 +0530

    feat: add initial HTML structure, main application component, and styles

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import Checkout from "./pages//checkout/CheckoutPage";
+import Orders from "./pages/OrderPage";
+import Tracking from "./pages/Tracking";
+import NotFoundPage from "./pages/NotFoundPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/orders" element={<Orders />} />
+      <Route path="/tracking" element={<Tracking />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+}
+
+export default App;
